d7095d9898c7304f55a485edd58e5daf7ea78c50
kush sharma <kush.73888@gmail.com>
2024-07-04 02:37:37 +0530

    MongoDB Setup

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isConnected?: number
+}
+
+const connection: ConnectionObject = {};
+
+export async function dbConnect(): Promise<void> {
+    if (connection.isConnected) {
+        console.log("Already connected");
+        return;
+    }
+
+    try {
+
+        const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
+        console.log(db);
+
+        connection.isConnected = db.connections[0].readyState
+        console.log(db.connections);
+        
+
+        console.log("Database connected successfully");
+
+    } catch (error) {
+
+        console.log("Database connection failed", error);
+        process.exit(1);
+
+    }
+}
