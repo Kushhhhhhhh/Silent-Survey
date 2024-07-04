c11b215e239d0da2403d6141c599f43931ff722d
kush sharma <kush.73888@gmail.com>
2024-07-05 01:16:24 +0530

    Middleware Setup

---
+++ src/middleware/middleware.ts
@@ -0,0 +1,38 @@
+import { NextResponse, NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export { default } from "next-auth/middleware";
+
+export async function middleware(request: NextRequest) {
+
+    const token = await getToken({ req: request });
+    const url = request.nextUrl;
+
+    if(token && (
+        url.pathname.startsWith('/sign-in') ||
+        url.pathname.startsWith('/sign-up') ||
+        url.pathname.startsWith('/verify') ||
+        url.pathname.startsWith('/')
+    )){
+        return NextResponse.redirect(new URL('/dashboard', request.url))
+    } 
+
+    if(!token && (
+        url.pathname.startsWith('/dashboard') ||
+        url.pathname.startsWith('/')
+    )){
+        return NextResponse.redirect(new URL('/sign-in', request.url))
+    }
+
+    return NextResponse.redirect(new URL("/", request.url));
+}
+
+export const config = {
+    matcher: [
+        "/sign-in",
+        "/sign-up",
+        "/",
+        "/dashboard/:path*",
+        "/verify/:path*"
+    ],
+}
