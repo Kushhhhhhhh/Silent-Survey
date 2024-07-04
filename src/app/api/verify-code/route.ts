013ce138174ec5ea58990c76fd5728d0e0141154
kush sharma <kush.73888@gmail.com>
2024-07-05 01:18:35 +0530

    Verify Code Route

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,62 @@
+import { dbConnect } from "@/lib/dbConnect";
+import { UserModel } from "@/model/User";
+
+export async function POST(request: Request) {
+    await dbConnect();
+    try {
+        const { username, code } = await request.json();
+        const decodedUsername = decodeURIComponent(username);
+        const user = await UserModel.findOne({ username: decodedUsername });
+        if (!user) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "User not found",
+                },
+                { status: 404 }
+            );
+        }
+
+        const isCodeValid = user.verifyCodeExpiry === code;
+        const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+
+        if (isCodeValid && isCodeNotExpired) {
+            user.isVerified = true;
+            await user.save();
+
+            return Response.json(
+                {
+                    success: true,
+                    message: "User verified successfully",
+                },
+                { status: 200 }
+            )
+        } else if (!isCodeNotExpired) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "Verification Code has expired , Please request a new code by Signing up again",
+                },
+                { status: 400 }
+            );
+        } else {
+            return Response.json(
+                {
+                    success: false,
+                    message: "Invalid verification code",
+                },
+                { status: 400 }
+            );
+        }
+
+    } catch (error) {
+        console.log("Error verifying user", error);
+        return Response.json(
+            {
+                success: false,
+                message: "Failed to verify user",
+            },
+            { status: 500 }
+        );
+    }
+}
