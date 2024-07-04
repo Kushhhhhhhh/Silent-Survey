0dbb4c37857491e9187033849962dc9c574a509d
kush sharma <kush.73888@gmail.com>
2024-07-05 01:18:08 +0530

    Unique Username Route

---
+++ src/app/api/username-unique/route.ts
@@ -0,0 +1,59 @@
+import { z } from "zod";
+import { dbConnect } from "@/lib/dbConnect";
+import { UserModel } from "@/model/User";
+import { usernameValidation } from "@/schema/signup-schema";
+
+const usernameQuerySchema = z.object({
+    username: usernameValidation
+})
+
+export async function GET(request: Request) {
+
+    await dbConnect();
+
+    try {
+
+        const { searchParams } = new URL(request.url);
+        const queryParam = {
+            username: searchParams.get("username")
+        }
+        const result = usernameQuerySchema.safeParse(queryParam);
+        console.log(result);
+
+        if (!result.success) {
+            const usernameErrors = result.error.format().username?._errors || [];
+
+            return Response.json({
+                success: false,
+                message: "Invalid username",
+                errors: usernameErrors
+            },
+            { status: 400 });
+        }
+
+        const { username } = result.data;
+
+        const existingVerifiedUser = await UserModel.findOne({
+            username, 
+            isVerified: true
+        })
+
+        if (existingVerifiedUser) {
+            return Response.json({
+                success: false,
+                message: "Username already exists"
+            },
+            { status: 400 });
+        }
+
+        return Response.json({ success: true, message: "Username available" });
+
+    } catch (error: any) {
+        console.log("Error checking Username", error);
+        return Response.json({
+            success: false,
+            message: "Error checking Username"
+        },
+            { status: 500 });
+    }
+}
